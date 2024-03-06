af9e8d1f4a9f7bd91cd3e9b76382f5495a4a354f
soumy <soumyaaitmbark@gmail.com>
2024-03-07 00:41:30 +0100

    commit message

--- src/main/java/metier/IMetierImpl.java
+++ src/main/java/metier/IMetierImpl.java
@@ -1,15 +1,11 @@
 package metier;
-
 import dao.IDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 @Component("metier")
 public class IMetierImpl implements IMetier {
     //couplage faible
-
     private IDao dao;
-
     public IMetierImpl(IDao dao){
         this.dao=dao;
     }
@@ -19,7 +15,6 @@ public class IMetierImpl implements IMetier {
         double res=tmp*540/Math.cos(tmp*Math.PI);
         return res;
     }
-
     public void setDao(IDao dao) {
         this.dao = dao;
     }
